34da5955ff3a86bdc32c35845e9a7bdaf2fc7893
LetsGaming <bergmanncrafter@gmail.com>
2023-07-11 02:28:52 +0200

    working on an improvement for color chase

--- led/animations/customAnimations.py
+++ led/animations/customAnimations.py
@@ -21,15 +21,15 @@ class Color_Wipe(Animation):
                     for i in range(self.strip.numPixels()):
                         if self.stopAnimation:
                             break
-                        self.strip.setPixelColor(i, color)
-                        self.strip.show()
-                        time.sleep(0.05)
+                        self.strip.setPixelColor(i, color)  # Set pixel color to the specified color
+                        self.strip.show()  # Update the LED strip with the new color
+                        time.sleep(0.05)  # Pause for a short duration
                     for i in range(self.strip.numPixels()):
                         if self.stopAnimation:
                             break
-                        self.strip.setPixelColor(i, 0)
-                        self.strip.show()
-                        time.sleep(0.05)
+                        self.strip.setPixelColor(i, 0)  # Set pixel color to black (turn off the pixel)
+                        self.strip.show()  # Update the LED strip
+                        time.sleep(0.05)  # Pause for a short duration
             else:
                 return False
         except Exception as e:
@@ -61,15 +61,15 @@ class Theater_Chase(Animation):
                             for i in range(0, self.strip.numPixels(), 3):
                                 if self.stopAnimation:
                                     break
-                                self.strip.setPixelColor(i + q, color)
-                            self.strip.show()
-                            time.sleep(0.05)
+                                self.strip.setPixelColor(i + q, color)  # Set color to the pixel
+                            self.strip.show()  # Update the LED strip
+                            time.sleep(0.05)  # Pause for a short duration
                             if self.stopAnimation:
                                 break
                             for i in range(0, self.strip.numPixels(), 3):
                                 if self.stopAnimation:
                                     break
-                                self.strip.setPixelColor(i + q, 0)
+                                self.strip.setPixelColor(i + q, 0)  # Set pixel color to black (turn off the pixel)
             else:
                 return False
         except Exception as e:
@@ -97,8 +97,8 @@ class Strobe(Animation):
                         if self.stopAnimation:
                             break
                         self.strip.setPixelColor(i, color)  # Set all pixels to the specified color
-                        self.strip.show()
-                        time.sleep(.5)
+                        self.strip.show()  # Update the LED strip
+                        time.sleep(.5)  # Pause for a short duration
                         for _ in range(5):
                             if self.stopAnimation:
                                 break
@@ -106,16 +106,16 @@ class Strobe(Animation):
                                 if self.stopAnimation:
                                     break
                                 self.strip.setPixelColor(i, 0)  # Turn off all pixels
-                            self.strip.show()
-                            time.sleep(.5)
+                            self.strip.show()  # Update the LED strip
+                            time.sleep(.5)  # Pause for a short duration
                             if self.stopAnimation:
                                 break
                             for i in range(num_pixels):
                                 if self.stopAnimation:
                                     break
                                 self.strip.setPixelColor(i, color)  # Turn on all pixels
-                            self.strip.show()
-                            time.sleep(.5)
+                            self.strip.show()  # Update the LED strip
+                            time.sleep(.5)  # Pause for a short duration
                             if self.stopAnimation:
                                 break
             else:
@@ -132,36 +132,38 @@ class Color_Chase(Animation):
         self.red = red
         self.green = green
         self.blue = blue
-        self.current_pixel = 0
-        self.tail_length = random.randint(1, 5)  # Random tail length between 1 and 5 pixels
+        # Animation parameters
+        self.MIN_PIXELS = 5        # Minimum number of pixels to start moving
+        self.MAX_PIXELS = 15       # Maximum number of pixels to start moving
+        self.MIN_DELAY = 0.5       # Minimum delay between new pixel groups (in seconds)
+        self.MAX_DELAY = 3.0       # Maximum delay between new pixel groups (in seconds)
 
     def color_chase(self):
-        """Chase a single color along the LED strip."""
+        pixels = []  # List to store the current pixel positions
         try:
             if validate_rgb_values(self.red, self.green, self.blue):
                 color = Color(self.red, self.green, self.blue)
                 num_pixels = self.strip.numPixels()
-                self.animationStarted = True
                 while not self.stopAnimation:
-                    # Update tail
-                    tail_start = max(self.current_pixel - self.tail_length, 0)
-                    tail_end = self.current_pixel - 1
-                    for i in range(tail_start, tail_end + 1):
-                        self.strip.setPixelColor(i, color)
-                    # Update current pixel
-                    self.strip.setPixelColor(self.current_pixel, color)
-                    if self.current_pixel > 0:
-                        self.strip.setPixelColor(self.current_pixel - 1, 0)  # Clear previous pixel
-                    self.strip.show()
-                    time.sleep(0.08)
-                    if self.stopAnimation:
-                        break
-                    if random.random() < 0.2:  # Randomly introduce a new pixel at the beginning
-                        self.strip.setPixelColor(num_pixels - 1, 0)  # Clear last pixel
-                        self.current_pixel = 0
-                        self.tail_length = random.randint(1, 5)  # Random tail length between 1 and 5 pixels
-                    else:
-                        self.current_pixel = (self.current_pixel + 1) % num_pixels  # Move current pixel forward
+                    # Move existing pixels
+                    for i in range(len(pixels)):
+                        pixels[i] += 1  # Increment pixel position
+                        if pixels[i] >= num_pixels:
+                            pixels[i] = num_pixels - 1  # Wrap around to the last pixel
+
+                    # Add new pixels randomly
+                    num_new_pixels = random.randint(self.MIN_PIXELS, self.MAX_PIXELS)
+                    new_pixels = [random.randint(0, num_pixels - 1) for _ in range(num_new_pixels)]
+                    pixels.extend(new_pixels)
+
+                    # Render pixels on the LED strip
+                    for pixel in pixels:
+                        self.strip.setPixelColor(pixel, color)  # Set color to white (you can change it to any color)
+                    self.strip.show()  # Update the LED strip
+
+                    # Delay before the next update
+                    delay = random.uniform(self.MIN_DELAY, self.MAX_DELAY)
+                    time.sleep(delay)  # Pause for a random duration
             else:
                 return False
         except Exception as e:
